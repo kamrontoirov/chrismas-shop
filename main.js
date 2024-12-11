69bbb1e43e6996e09926f9f9f5ae33ce8e8e743f
kamrontoirov <toirovkamron000@gmail.com>
2024-12-11 17:59:57 +0500

    424px responsive taxlandi

--- main.js
+++ main.js
@@ -4,3 +4,20 @@ burgerBtn.addEventListener("click", () => {
     burgerBtn.classList.toggle("active");
     headerNav.classList.toggle("mobile__nav");
 });
+function timer() {
+    const now = new Date();
+    const newYear = new Date(now.getFullYear() + 1, 0, 1);
+    const diff = newYear - now;
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((diff % (1000 * 60)) / 1000);
+
+    document.querySelector(".day").textContent = days;
+    document.querySelector(".hour").textContent = hours;
+    document.querySelector(".minute").textContent = minutes;
+    document.querySelector(".second").textContent = seconds;
+}
+
+setInterval(timer, 1000);
